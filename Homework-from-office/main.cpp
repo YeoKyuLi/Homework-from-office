fda0e6231b9f9e5cae5a0c0fb75a6cf4f6925298
YeoKyuLi <kyuli.yeo@gmail.com>
2019-05-14 22:13:08 +0900

    Initial Commit

---
+++ Homework-from-office/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework-from-office
+//
+//  Created by Yeo Kyu Li on 14/05/2019.
+//  Copyright © 2019 Yeo Kyu Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
