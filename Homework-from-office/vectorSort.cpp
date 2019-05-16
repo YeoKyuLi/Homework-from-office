463cff09913319c96e276c1e4ac24c85c48b3390
YeoKyuLi <kyuli.yeo@gmail.com>
2019-05-16 21:14:24 +0900

    adding sorting

--- Homework-from-office/vectorSort.cpp
+++ Homework-from-office/vectorSort.cpp
@@ -17,35 +17,45 @@ using namespace std;
 
 struct Data{
     int x,y;
-    static int sort_by;
+    int sortC;
     bool operator<(const Data& rhs)const{
-        if(sort_by == 1) return x < rhs.x;
-        else return y < rhs.y;
+        return (x < rhs.x);
     }
+
 };
-int Data::sort_by=1;
+
+bool decrease(int a, int b){
+    return a > b;
+}
+
 int main()
 {
     //---------------------------------- int -----------------------------------------//
     vector<int> arr = {1,3,4,2,5,1,77,224,5,6};
-    vector<Data> structD = {{1,2}, {200,50}, {100,6}};
+    vector<Data> structD = {{1,65}, {200,50}, {100,6}};
     
     sort(arr.begin(), arr.end());
     for(auto value : arr)
         cout << value << " ";
     cout << endl;
     
-    reverse(arr.begin(), arr.end());
+    sort(arr.begin(), arr.end(), decrease);
     for(auto value : arr)
         cout << value << " ";
     cout << endl;
     
     sort(structD.begin(), structD.end());
-    Data::sort_by=2;
-    sort(structD.begin(), structD.end());
     for(int x = 0 ; x < structD.size(); x++)
         cout << "{ " << structD[x].x << ", " << structD[x].y << " }" << " ";
+    cout << endl;
+ 
     
+    sort(structD.begin(), structD.end(), [](const Data& lhs, const Data& rhs) {
+        return lhs.y < rhs.y;
+    });
+    
+    for(auto x = structD.begin(); x != structD.end(); x++)
+        cout << "{ " << x -> x << ", " << x -> y << " } " << " " ;
     cout << endl;
     //---------------------------------- char -----------------------------------------//
     vector<char> arrC = {'F', 'A', 'C', 'F', 'H', 'O', 'C', 'A'};
