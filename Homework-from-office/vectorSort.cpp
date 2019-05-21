4fe6096d22d41fca4901b7a419c41c80bafd1ef5
Kyuli Yeo <kyuli.yeo@ericsson.com>
2019-05-21 14:05:33 +0900

    Added New homework

--- Homework-from-office/vectorSort.cpp
+++ Homework-from-office/vectorSort.cpp
@@ -16,6 +16,9 @@ using namespace std;
 // operator에 람다 넣어보기
 // functor
 // 람다
+// operator less
+// Map :: "나이", "키"를 넣었을때 사람이름 나오도록
+//  char name[100];  <- key .. Data  <- value;
 /*
 람다의 사용법은 간단하다. 
 
@@ -36,21 +39,22 @@ struct Data{
 //        return (x < rhs.x);
 //    }
 public :
-    Data(int a=0, int b=0): x(a), y(b) {}
+    // Data(int a=0, int b=0): x(a), y(b) {}
     bool operator<(const Data& rhs)const{
-        return x < rhs.x ;
+        if (x == rhs.x) return y < rhs.y;
+        return (x < rhs.x);
     }
 
 //    bool by_x(const Data& rhs)const{ return (x<rhs.x); };
 //    bool by_y(const Data& rhs)const{ return (y<rhs.y); };
-    Data operator<(const Data& rhs);
+    // Data operator<(const Data& rhs);
 
 
 };
-Data Data::operator<(const Data& rhs){
-        // cout << (x < rhs.x) << " " << (y < rhs.y) << endl;
-        return (x < rhs.x) && (y < rhs.y);
-}
+// Data Data::operator<(const Data& rhs){
+//         // cout << (x < rhs.x) << " " << (y < rhs.y) << endl;
+//         return (x < rhs.x) && (y < rhs.y);
+// }
 
 
 bool sortbysecdesc(const pair<int,int> &a,
@@ -90,37 +94,38 @@ int main()
     // Sorting reverse, using functor
 //    sort(seq_arr.begin(), seq_arr.end(), greater<int>());
 
-    sort(seq_arr.begin(), seq_arr.end());
+    // sort(seq_arr.begin(), seq_arr.end());
 
 //    sort(seq_arr.begin(), seq_arr.end(), std::less<int>());
-    for(auto x = seq_arr.begin(); x != seq_arr.end(); x++)
-        cout << x->x << " " ;
-    cout << endl;
-    
+    // for(auto x = seq_arr.begin(); x != seq_arr.end(); x++)
+    //     cout << x->x << " " ;
+    // cout << endl;
+
+    sort(structD.begin(), structD.end());
     // sorting lambda
-    sort(structD.begin(), structD.end(), [](const Data& lhs, const Data& rhs) {
-        return lhs.y < rhs.y;
-    });
+    // sort(structD.begin(), structD.end(), [](const Data& lhs, const Data& rhs) {
+    //     return lhs.y < rhs.y;
+    // });
     for(auto x = structD.begin(); x != structD.end(); x++)
         cout << "{ " << x -> x << ", " << x -> y << " } " << " " ;
     cout << endl;
 
 //    //------------------- sorting vector<<int,int>> -------------------------------//
-    sort(structD.begin(), structD.end());
+    // sort(structD.begin(), structD.end());
 
 
-    for(auto x = structD.begin(); x != structD.end(); x++)
-        cout << "{ " << x -> x << ", " << x -> y << " } " << " " ;
-    cout << endl;
-    sort(arr_s.begin(), arr_s.end());
-    for(auto x = 0; x < arr_s.size(); x++)
-        cout << "{ " << arr_s[x].first << ", " <<  arr_s[x].second << " } " << " " ;
-    cout << endl;
+    // for(auto x = structD.begin(); x != structD.end(); x++)
+    //     cout << "{ " << x -> x << ", " << x -> y << " } " << " " ;
+    // cout << endl;
+    // sort(arr_s.begin(), arr_s.end());
+    // for(auto x = 0; x < arr_s.size(); x++)
+    //     cout << "{ " << arr_s[x].first << ", " <<  arr_s[x].second << " } " << " " ;
+    // cout << endl;
     
-    sort(arr_s.begin(), arr_s.end(), sortbysecdesc);
-    for(auto x = 0; x < arr_s.size(); x++)
-        cout << "{ " << arr_s[x].first << ", " <<  arr_s[x].second << " } " << " " ;
-    cout << endl;
+    // sort(arr_s.begin(), arr_s.end(), sortbysecdesc);
+    // for(auto x = 0; x < arr_s.size(); x++)
+    //     cout << "{ " << arr_s[x].first << ", " <<  arr_s[x].second << " } " << " " ;
+    // cout << endl;
 //    //------------------- sorting vector<<int,int>> -------------------------------//
     
 //    sort(arr.begin(), arr.end());
